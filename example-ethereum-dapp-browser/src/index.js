dcaf6b89567a675825f383869466c65c0dabfbfa
machard <matthieu__@hotmail.fr>
2021-03-18 15:57:05 +0100

    update

--- example-ethereum-dapp-browser/src/index.js
+++ example-ethereum-dapp-browser/src/index.js
@@ -5,11 +5,25 @@ const transport = new Transport();
 window.ledgerlive = makeLedgerlive(transport);
 transport.connect();
 
-var iframe = document.createElement('iframe');
-iframe.style.height = "100%";
-iframe.style.width = "100%";
-iframe.src = "http://app.aave.com";
-iframe.onload = () => {
-  iframe.contentWindow.ethereum = { send: () => alert("coucou") };
+window.launch = (url) => {
+  const exIframe = document.querySelector("iframe");
+  if (exIframe) {
+    exIframe.remove();
+  }
+
+  const iframe = document.createElement('iframe');
+  iframe.style.height = "100%";
+  iframe.style.width = "100%";
+  iframe.src = url;
+  iframe.onload = () => {
+    iframe.contentWindow.ethereum = { send: () => alert("coucou") };
+  }
+  document.body.appendChild(iframe);
 }
-document.body.appendChild(iframe);
+
+const urlParams = new URLSearchParams(window.location.search);
+const url = urlParams.get('app');
+if (url) {
+  window.launch(url);
+  document.querySelector("#nav").remove();
+}
