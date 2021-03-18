5f462dafd9dcad412510eaa2a1136cdef57cb74c
machard <matthieu__@hotmail.fr>
2021-03-18 15:42:50 +0100

    poc dappbrowser ok

--- example-ethereum-dapp-browser/src/index.js
+++ example-ethereum-dapp-browser/src/index.js
@@ -4,3 +4,12 @@ import Transport from "../../src/transport";
 const transport = new Transport();
 window.ledgerlive = makeLedgerlive(transport);
 transport.connect();
+
+var iframe = document.createElement('iframe');
+iframe.style.height = "100%";
+iframe.style.width = "100%";
+iframe.src = "http://app.aave.com";
+iframe.onload = () => {
+  iframe.contentWindow.ethereum = { send: () => alert("coucou") };
+}
+document.body.appendChild(iframe);
